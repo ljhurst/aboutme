7be0b6d0821cd9779f0912bdd0abbb29992ae425
Luke Hurst <hurstlj@umich.edu>
2016-07-09 00:43:59 -0700

    Add function to request top tracks from Spotify

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -1,5 +1,33 @@
+var request = require('request');
+
+var topTracks = [];
+
 function getTopTracks(request, response) {
-    response.send('Tunes!!');
+    response.send(topTracks);
 }
+
+function requestTopTracks() {
+    request({
+        url: 'https://api.spotify.com/v1/me/top/tracks',
+        auth: {
+            bearer: 'BQCwJs4pRTkuD815_xtveGQDC1W_nNKaO03e1-cwaQiihJVVX7SPbeoCO5fc48fsPfwfxY_QYivWR1AAmFS_9lPsi5eqtD6zKkFaVRrR213Tf_ByC1NM6pbLowwwisEtFNLwKY4vIeGQptJjLF4',
+        },
+        qs: {
+            time_range: 'short_term',
+            limit: 5,
+        },
+    },
+    function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(body);
+            body = JSON.parse(body);
+            topTracks = body.items;
+        }
+    });
+}
+
+requestTopTracks();
 
 module.exports.getTopTracks = getTopTracks;
