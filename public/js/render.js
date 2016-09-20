7c7fa90004039ed2a6ea77bedd1f55e8a883036b
Luke Hurst <hurstlj@umich.edu>
2016-09-20 10:57:22 -0400

    Add rendering for contribution template

--- public/js/render.js
+++ public/js/render.js
@@ -1,7 +1,10 @@
 $(function () {
-    $.ajax({url: '/top-tracks'})
-        .done(function (data) {
-            var songsTemplate = Handlebars.compile($('#song-template').html());
-            $('#top-tracks-list').html(songsTemplate({songs: data}));
-        });
+    $.ajax({url: '/top-tracks'}).done(function (data) {
+        var songsTemplate = Handlebars.compile($('#song-template').html());
+        $('#top-tracks-list').html(songsTemplate({songs: data}));
+    });
+    $.ajax({url: '/current-contribs'}).done(function (data) {
+        var contribsTemplate = Handlebars.compile($('#contrib-template').html());
+        $('#current-contribs-list').html(contribsTemplate({contribs: data}));
+    });
 });
