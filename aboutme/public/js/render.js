81caacea869fd41b221920d7e90ae31b4b48d8b1
Luke Hurst <luke.hurst@nike.com>
2018-08-10 20:58:10 -0700

    Add loading content for current contributions

--- aboutme/public/js/render.js
+++ aboutme/public/js/render.js
@@ -1,10 +1,30 @@
 $(function () {
+    // Create filler
+    const fillerContrib = {
+        'actor': {
+            'login': 'Loading...',
+            'url': '#'
+        },
+        'repo': {
+            'name': 'Loading...',
+            'url': '#'
+        },
+    };
+
+    const fillerContribs = new Array(5).fill().map(u => fillerContrib);
+
+    // Get templates
+    let songsTemplate = Handlebars.compile($('#song-template').html());
+    let contribsTemplate = Handlebars.compile($('#contrib-template').html());
+
+    // Render filler data
+    $('#current-contribs-list').html(contribsTemplate({contribs: fillerContribs}));
+
+    // Render real data
     $.ajax({url: '/top-tracks'}).done(function (data) {
-        var songsTemplate = Handlebars.compile($('#song-template').html());
         $('#top-tracks-list').html(songsTemplate({songs: data}));
     });
     $.ajax({url: '/current-contribs'}).done(function (data) {
-        var contribsTemplate = Handlebars.compile($('#contrib-template').html());
         $('#current-contribs-list').html(contribsTemplate({contribs: data}));
     });
 });
