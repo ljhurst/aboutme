5655b2b062e873987a68fbda0fbb529d80e35056
Luke Hurst <luke.hurst@nike.com>
2018-08-14 22:42:16 -0700

    Add filler tracks

--- aboutme/public/js/render.js
+++ aboutme/public/js/render.js
@@ -1,16 +1,37 @@
 $(function () {
     // Create filler
+    const FILLER_OADING_TEXT = 'Loading...';
+    const FILLER_HREF = '#';
+
+    const fillerTrack = {
+        'album': {
+            'external_urls': { 'spotify': FILLER_HREF },
+            'images': [{
+                'height': 640,
+                'width': 640,
+                'url': './images/Spotify_Icon_RGB_Black.png'
+            }]
+        },
+        'artists': [{
+            'external_urls': { 'spotify': FILLER_HREF },
+            'name': FILLER_OADING_TEXT
+        }],
+        'external_urls': { 'spotify': FILLER_HREF },
+        'name': FILLER_OADING_TEXT
+    }
+
     const fillerContrib = {
         'actor': {
-            'login': 'Loading...',
-            'url': '#'
+            'login': FILLER_OADING_TEXT,
+            'url': FILLER_HREF
         },
         'repo': {
-            'name': 'Loading...',
-            'url': '#'
+            'name': FILLER_OADING_TEXT,
+            'url': FILLER_HREF
         },
     };
 
+    const fillerTracks = new Array(5).fill().map(u => fillerTrack);
     const fillerContribs = new Array(5).fill().map(u => fillerContrib);
 
     // Get templates
@@ -18,6 +39,7 @@ $(function () {
     let contribsTemplate = Handlebars.compile($('#contrib-template').html());
 
     // Render filler data
+    $('#top-tracks-list').html(songsTemplate({songs: fillerTracks}));
     $('#current-contribs-list').html(contribsTemplate({contribs: fillerContribs}));
 
     // Render real data
