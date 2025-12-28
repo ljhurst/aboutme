b6fbfd1c2aa6a0026f798f9eb7a495112ef0be24
Luke Hurst <hurstlj@umich.edu>
2025-12-28 02:44:58 -0800

    Refresh style with Tailwind CSS

--- aboutme/public/js/helper.js
+++ aboutme/public/js/helper.js
@@ -1,10 +1,38 @@
 /*global Handlebars*/
 
-(function () {
+(() => {
     Handlebars.registerHelper('if_plural', function (arg, options) {
         if (arg !== 1) {
             return options.fn(this);
         }
         return options.inverse(this);
+    });
+
+    Handlebars.registerHelper('time_ago', function (timestamp) {
+        const now = new Date();
+        const past = new Date(timestamp);
+
+        const elapsedSeconds = Math.floor((now - past) / 1000);
+
+        const intervalsInSeconds = {
+            year: 31536000,
+            month: 2592000,
+            week: 604800,
+            day: 86400,
+            hour: 3600,
+            minute: 60,
+        };
+
+        for (let [unit, secondsInUnit] of Object.entries(intervalsInSeconds)) {
+            const unitRatio = Math.floor(elapsedSeconds / secondsInUnit);
+
+            if (unitRatio === 1) {
+                return `1 ${unit} ago`;
+            } else if (unitRatio > 1) {
+                return `${unitRatio} ${unit}s ago`;
+            }
+        }
+
+        return 'just now';
     });
 })();
