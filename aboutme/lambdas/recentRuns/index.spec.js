d12691786cf821ff86a9aa75be7771e1b58f0904
Luke Hurst <hurstlj@umich.edu>
2026-01-06 00:50:21 -0800

    Bump versions

--- aboutme/lambdas/recentRuns/index.spec.js
+++ aboutme/lambdas/recentRuns/index.spec.js
@@ -3,8 +3,8 @@ import { handler } from './index.js';
 
 const log = createDebug('recent-runs:test');
 
-//const noLimit = { queryStringParameters: null };
+const noLimit = { queryStringParameters: null };
 const limit5 = { queryStringParameters: { limit: 5 } };
 
-//handler(noLimit).then((data) => log(data));
+handler(noLimit).then((data) => log(data));
 handler(limit5).then((data) => log(JSON.parse(data.body)));
