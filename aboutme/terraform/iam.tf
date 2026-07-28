1041e448bd55a02191d9daa390003ea55aa195ec
Luke Hurst <hurstlj@umich.edu>
2026-07-27 23:23:44 -0700

    Use OIDC roles for deploy

--- aboutme/terraform/iam.tf
+++ aboutme/terraform/iam.tf
@@ -32,3 +32,98 @@ resource "aws_iam_role_policy" "lambda_exec_logs" {
     ]
   })
 }
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+locals {
+  github_actions_oidc_condition = {
+    StringEquals = {
+      "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+    }
+    StringLike = {
+      "token.actions.githubusercontent.com:sub" = "repo:ljhurst/aboutme:ref:refs/heads/master"
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_backend_deploy" {
+  name = "lj-aboutme-github-actions-backend-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github_actions.arn
+      }
+      Condition = local.github_actions_oidc_condition
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy_lambda" {
+  name = "deploy-lambda-code"
+  role = aws_iam_role.github_actions_backend_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:UpdateFunctionCode",
+          "lambda:GetFunction"
+        ]
+        Resource = [
+          aws_lambda_function.current_contribs.arn,
+          aws_lambda_function.top_tracks.arn,
+          aws_lambda_function.recent_runs.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "github_actions_frontend_deploy" {
+  name = "lj-aboutme-github-actions-frontend-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github_actions.arn
+      }
+      Condition = local.github_actions_oidc_condition
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy_frontend" {
+  name = "deploy-frontend-site"
+  role = aws_iam_role.github_actions_frontend_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.static_site.arn,
+          "${aws_s3_bucket.static_site.arn}/*"
+        ]
+      }
+    ]
+  })
+}
--- aboutme/terraform/outputs.tf
+++ aboutme/terraform/outputs.tf
@@ -7,3 +7,13 @@ output "api_gateway_url" {
   description = "API Gateway endpoint URL"
   value       = aws_api_gateway_stage.aboutme.invoke_url
 }
+
+output "github_actions_backend_deploy_role_arn" {
+  description = "Role ARN for GitHub Actions to assume when deploying Lambda code"
+  value       = aws_iam_role.github_actions_backend_deploy.arn
+}
+
+output "github_actions_frontend_deploy_role_arn" {
+  description = "Role ARN for GitHub Actions to assume when deploying the frontend to S3"
+  value       = aws_iam_role.github_actions_frontend_deploy.arn
+}
